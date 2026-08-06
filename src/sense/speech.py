3e3a8528e29d6ab362730b2377eb9cf9e7733f56
happycoding0-0 <indexjw@gmail.com>
2026-08-06 16:42:01 +0900

    speech() 함수 한번만 재생되는거 못고쳤음. 일단 매번 새로운 엔진을 만드는 방식으로 카메라 두번열리는지 테스트해볼예정

--- src/sense/speech.py
+++ src/sense/speech.py
@@ -1,29 +1,39 @@
 # src\sense\speech.py
 
 import pyttsx3 as tts
-
+import time
 class talk():
     def __init__(self):
         
         self.engine = tts.init()
-    def speech(self,text):
-        self.engine = tts.init()
-        print("engine id:", id(self.engine))
-
-        if not text:
-            print('fuck')
-            exit()
-        self.engine.say(text)
-        self.engine.runAndWait()
         
+    def speech(self,text):
+        
+        if self.engine.isBusy():
+                                    print("busy1")
+        
+        self.engine.say(text)
+        if self.engine.isBusy():
+                                            print("busy2")
+                
+        self.engine.runAndWait()
+        self.engine.stop()
+        if self.engine.isBusy():
+                                            print("busy3")
+                
+        print(text)
+        print("끝")
 
 
 if __name__ == "__main__":
     speech = talk()
-    speech.speech("첫번째")
-    print("첫번째 완료")
-    speech.speech("두번째")
-    print("두번째 완료")
+    talk().speech("첫번째")
+    #speech.speech("첫번째")
+    # print("첫번째 완료")
+    
+    talk().speech("두번째")
+    #speech.speech("두번째")
+    # print("두번째 완료")
     
 
         
