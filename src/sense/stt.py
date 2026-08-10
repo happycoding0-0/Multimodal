074382aa2f1a30e22791dce49558e4a65dd6195d
easywoo <indexjw@gmail.com>
2026-08-10 23:23:56 +0900

    오디오 처리

--- src/sense/stt.py
+++ src/sense/stt.py
@@ -4,15 +4,35 @@ from path import model_download_root
 import sounddevice as sd
 import openwakeword 
 from openwakeword.model import Model
+import pyaudio
+import time
+import threading
 
+
+class Listener:
+    def __init__(self, SAMPLE_RATE = 16000):
+        self.SAMPLE_RATE = SAMPLE_RATE
+        self.CHUNK_SIZE = 1280
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format = pyaudio.paInt16, channels= 1, rate = self.SAMPLE_RATE, input=True , output=True, frames_per_buffer=self.CHUNK_SIZE)
+
+    def listen(self,queue):
+        while True:
+            data = self.stream.read(self.CHUNK_SIZE, exception_on_overflow=False)
+            queue.append(data)
+            time.sleep(0.01)
+
+    def run(self, queue):
+        thread = threading.Thread(target= self.listen, args=(queue,),daemon=True)
+        thread.start()
+        print("Speech Recognition is now listening... \n")
 
 class Stt():
     def __init__(self):
         self.SAMPLE_RATE = 16000
-        self.CHUNK_SIZE = 1280 
-        
+        self.CHUNK_SIZE = 1280
         self.stt_model = whisper.load_model(name = "small", download_root=model_download_root, in_memory=True)
-
+        
         #openwakeword.utils.download_models(target_directory=f"{model_download_root}/openwakeword_models")
         self.wakeword_model = Model(
             wakeword_models=[f"{model_download_root}/openwakeword_models/hey_jarvis_v0.1.onnx"],
@@ -21,37 +41,43 @@ class Stt():
             inference_framework="onnx"
             )
 
+        self.listener = Listener(SAMPLE_RATE=16000)
+        self.audio_q = list()
+
     def wake_word(self):
-        with sd.InputStream(samplerate=self.SAMPLE_RATE , channels= 1, dtype='int16') as stream:
-            print("Listening...")
-            while True:
-                audio_chunk, _ = stream.read(self.CHUNK_SIZE)
-                audio_chunk =audio_chunk.flatten()
-                self.prediction = self.wakeword_model.predict(audio_chunk)
+        pass
+        # 구버전
+        # with sd.InputStream(samplerate=self.SAMPLE_RATE , channels= 1, dtype='int16') as stream:
+        #     print("Listening...")
+        #     while True:
+        #         audio_chunk, _ = stream.read(self.CHUNK_SIZE)
+        #         audio_chunk =audio_chunk.flatten()
+        #         self.prediction = self.wakeword_model.predict(audio_chunk)
 
-                for wake_word, score in self.prediction.items():
+        #         for wake_word, score in self.prediction.items():
 
-                    if score >=0.5:
-                        print(wake_word, score)
+        #             if score >=0.5:
+        #                 print(wake_word, score)
                         
-                        return self.stt()
+        #                 #return self.stt()
+        #                 return  self.lisenter
         
         
-    def get_audio(self,duration = 5):
-        print("recording...")
-        self.audio = sd.rec(
-            int(duration* self.SAMPLE_RATE),
-            samplerate=self.SAMPLE_RATE,
-            channels=1,
-            dtype="float32"
-                            )
-        sd.wait()
-        print("end recording")
-        return self.audio.flatten()
+    # def get_audio(self,duration = 5):
+    #     print("recording...")
+    #     self.audio = sd.rec(
+    #         int(duration* self.SAMPLE_RATE),
+    #         samplerate=self.SAMPLE_RATE,
+    #         channels=1,
+    #         dtype="float32"
+    #                         )
+    #     sd.wait()
+    #     print("end recording")
+    #     return self.audio.flatten()
 
     def stt(self):
-        audio = self.get_audio(duration= 5)
-
+        #audio = self.get_audio(duration= 5)
+        
         audio = whisper.pad_or_trim(audio)
         mel = whisper.log_mel_spectrogram(audio, n_mels=self.stt_model.dims.n_mels).to(self.stt_model.device)
         _, probs = self.stt_model.detect_language(mel)
@@ -59,9 +85,29 @@ class Stt():
         result = whisper.decode(self.stt_model, mel, options)
         print(result.text)
 
+    def inference_loop(self):
+        while True:
+            if len(self.audio_q) < 5:
+                continue
+            else:
+                pred_q = self.audio_q.copy()
+                self.audio_q.clear()
+                print(pred_q)
+            time.sleep(0.05)
+
+    def run(self):
+        self.listener.run(self.audio_q)
+        thread = threading.Thread(target=self.inference_loop,
+                                    # args=(action,)
+                                    daemon=True)
+        thread.start()
+
+    
+
 if __name__ == "__main__":
     stt = Stt()
-    stt.wake_word()
+    stt.run()
+    #stt.wake_word()
     #print(f"{model_download_root}/openwakeword_models/hey_jarvis_v0.1.onnx")
 
     
