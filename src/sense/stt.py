d6aec01db4ec3d50aaebf20e814310fa2ef35f53
easywoo <indexjw@gmail.com>
2026-08-13 02:32:36 +0900

    stt 버퍼 구현

--- src/sense/stt.py
+++ src/sense/stt.py
@@ -1,98 +1,43 @@
 
-from path import model_download_root
-import sounddevice as sd
-import openwakeword 
-from openwakeword.model import Model
+
 import pyaudio
-import time
-import threading
-from faster_whisper import WhisperModel
 import numpy as np
-
 from collections import deque
+import threading 
 
 
-class Listener:
-    def __init__(self):
-        self.SAMPLE_RATE =  16000
-        self.CHUNK_SIZE = 1280
-        self.FORMAT = pyaudio.paInt16
-        self.p = pyaudio.PyAudio()
-    
-        self.stream = self.p.open(format = self.FORMAT, channels= 1, rate = self.SAMPLE_RATE, input=True , frames_per_buffer=self.CHUNK_SIZE)
+p = pyaudio.PyAudio()
+stream = p.open(rate=16000,channels=1,format=pyaudio.paInt16, input=True,frames_per_buffer=1024)
+q = deque()
+q_buffer = deque()
 
-    def mic_data(self,queue):
-        while True:
-            raw_data = self.stream.read(self.CHUNK_SIZE)
-            data = np.frombuffer(raw_data,dtype=np.int16)
-            queue.append(data)
-            time.sleep(0.05)
-            
+def mic(q):
+    while True:
+        raw_data = stream.read(num_frames=1024)
+        data = np.frombuffer(raw_data,dtype=np.int16)
+        q.append(data)
+        # volume = np.abs(data).mean()
+        # print(int(volume))
 
+def stt(q):
 
-class Stt():
-
-    def __init__(self):
-        self.is_running = True
-        self.SAMPLE_RATE = 16000
-        self.CHUNK_SIZE = 1280
-        self.mic_queue = deque()
-        
-        # openwakeword.utils.download_models(target_directory=f"{model_download_root}/openwakeword_models")
-        self.wakeword_model = Model(
-            wakeword_models=[f"{model_download_root}/openwakeword_models/hey_jarvis_v0.1.onnx"],
-            melspec_model_path=f"{model_download_root}/openwakeword_models/melspectrogram.onnx",
-            embedding_model_path=f"{model_download_root}/openwakeword_models/embedding_model.onnx",
-            inference_framework="onnx"
-            )
-        self.faster_whisper_model = WhisperModel("small",download_root= model_download_root,compute_type="int8")
-
-    def wake_word(self,queue):
-            print("Wake Word is Listening...")
-            while True:
-                if  queue:
-                    self.prediction = self.wakeword_model.predict( queue.popleft())
-                    for wake_word , score in self.prediction.items():
-                        if score >= 0.5:
-                            print(wake_word, score)
-
-                            return
-                        else:
-                            print("failed: ",wake_word,float(score))
-     
-                    
-        
-        
-
-    def stt(self,queue):        
-        
-        while True:
-                time.sleep(0.05)
+    while True:
+        if q:
+            if len(q) < 16*2: #  요소 개수 1개 = 1024 ,1024 * 15.625 = 16000 이며 16000 는 1s
+                print(len(q))
+                continue
+            else:
+                print(len(q))
+                q_buffer = np.concatenate(q)
                 
-                audio = np.concatenate(queue)
-                
-                
-                queue.clear()
-                
-                 
-                segments , info = self.faster_whisper_model.transcribe(language="ko",audio = audio,beam_size=5)
-                   
-                for segment in segments:
-                    print("STT: ","[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-
-        
+                q.clear()
+                # 이제 여기에 stt 모델 
+        else:
+            print("no q")        
         
 
 if __name__ == "__main__":
-    
-    listener = Listener()
-    stt = Stt()
-    t1 = threading.Thread(target=listener.mic_data, args=(stt.mic_queue,), daemon=True)
-    t1.start()
-    stt.stt(stt.mic_queue)
-    while True:
-        print("???")
-        time.sleep(10)
-    
-
-    
+    t = threading.Thread(target=mic, args=(q,),daemon=True)
+    t.start()
+    stt(q)
+    stream.close()
